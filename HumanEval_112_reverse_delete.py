9d39dc4b60a590b8b01ee2942c91b199fc50970e
Benjamin Grayzel <120680905+BGray-wrl@users.noreply.github.com>
2025-06-04 18:40:14 -0400

    Add HumanEval_112_reverse_delete.py

---
+++ HumanEval_112_reverse_delete.py
@@ -0,0 +1,19 @@
+def reverse_delete(s,c):
+    """Task
+    We are given two strings s and c, you have to deleted all the characters in s that are equal to any character in c
+    then check if the result string is palindrome.
+    A string is called palindrome if it reads the same backward as forward.
+    You should return a tuple containing the result string and True/False for the check.
+    Example
+    For s = "abcde", c = "ae", the result should be ('bcd',False)
+    For s = "abcdef", c = "b"  the result should be ('acdef',False)
+    For s = "abcdedcba", c = "ab", the result should be ('cdedc',True)
+    """
+    result_s = ""
+    for char_s in s:
+        if char_s not in c:
+            result_s += char_s
+
+    is_palindrome = (result_s == result_s[::-1])
+
+    return (result_s, is_palindrome)
