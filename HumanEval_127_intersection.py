db3124e73a90a3ee5ad704e015a14554d0028ee1
Benjamin Grayzel <120680905+BGray-wrl@users.noreply.github.com>
2025-06-04 18:43:53 -0400

    Implement HumanEval_127_intersection

---
+++ HumanEval_127_intersection.py
@@ -0,0 +1,53 @@
+def intersection(interval1, interval2):
+    """You are given two intervals,
+    where each interval is a pair of integers. For example, interval = (start, end) = (1, 2).
+    The given intervals are closed which means that the interval (start, end)
+    includes both start and end.
+    For each given interval, it is assumed that its start is less or equal its end.
+    Your task is to determine whether the length of intersection of these two 
+    intervals is a prime number.
+    Example, the intersection of the intervals (1, 3), (2, 4) is (2, 3)
+    which its length is 1, which not a prime number.
+    If the length of the intersection is a prime number, return "YES",
+    otherwise, return "NO".
+    If the two intervals don't intersect, return "NO".
+
+
+    [input/output] samples:
+    intersection((1, 2), (2, 3)) ==> "NO"
+    intersection((-1, 1), (0, 4)) ==> "NO"
+    intersection((-3, -1), (-5, 5)) ==> "YES"
+    """
+    start1, end1 = interval1
+    start2, end2 = interval2
+
+    # Calculate the intersection interval
+    intersection_start = max(start1, start2)
+    intersection_end = min(end1, end2)
+
+    # If there is no intersection, the length is 0
+    if intersection_start > intersection_end:
+        return "NO"
+
+    # Calculate the length of the intersection
+    length = intersection_end - intersection_start + 1
+
+    # Check if the length is a prime number
+    def is_prime(n):
+        if n <= 1:
+            return False
+        if n <= 3:
+            return True
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        i = 5
+        while i * i <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+    if is_prime(length):
+        return "YES"
+    else:
+        return "NO"
